b3f4efd7767149dfe0f7519b65b76fb027a91379
Scott <sfrancis@misinteractive.com>
2016-03-03 19:02:17 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+
+var gulp=require('gulp');
+var sassdoc = require('sassdoc');
+
+gulp.task('default',function(){
+    console.log('echo material design icons...');
+});
+
+
+
+gulp.task('sassdoc', function () {
+    return gulp.src('stylesheets/**/*.scss')
+        .pipe(sassdoc());
+
+});
